a631be107b89f4cd977ae2bd4430a6431f9f2a36
Rubendvb <rubenvasquez556@gmail.com>
2023-11-02 18:41:16 -0300

    adicionando switch

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const title = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
   alterarContexto('foco')
@@ -19,4 +20,36 @@ longoBt.addEventListener('click', () => {
 function alterarContexto(contexto) {
   html.setAttribute('data-contexto', contexto)
   banner.setAttribute('src', `/imagens/${contexto}.png`)
+
+  switch (contexto) {
+    case 'foco':
+      title.innerHTML = `
+       Otimize sua produtividade,<br />
+       <strong class="app__title-strong">
+        mergulhe no que importa.
+       </strong>`
+
+      break
+
+    case 'descanso-curto':
+      title.innerHTML = `
+       Que tal dar uma respirada? <br />
+       <strong class="app__title-strong">
+        Faça uma pausa curta!
+       </strong>`
+
+      break
+
+    case 'descanso-longo':
+      title.innerHTML = `
+       Hora de voltar à superfície.<br />
+       <strong class="app__title-strong">
+        Faça uma pausa longa.
+       </strong>`
+
+      break
+
+    default:
+      break
+  }
 }
